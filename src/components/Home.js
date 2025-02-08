9f11bdbc0a1258b34027e430fe90cf276d74d54e
Rohan Kumar <rohankumar0513@gmail.com>
2025-02-08 13:23:17 -0600

    changed styling to viewport

--- src/components/Home.js
+++ src/components/Home.js
@@ -116,15 +116,15 @@ const Home = () => {
   return (
     <div className="container">
         <div className="content">
-          <motion.div style={{paddingTop: '10%'}}>
-            <div style={{flexDirection: 'row', verticalAlign: "bottom", display: "inline-flex", height: '6rem'}}>
+          <motion.div className="text-container">
+            <div style={{flexDirection: 'row', verticalAlign: "bottom", display: "inline-flex"}}>
             <motion.h1  initial = {{ opacity: 0, y: 100 }} animate = {{ opacity: 1, y: 0 }} transition = {{ duration: 1.5, ease: "backIn", type: "spring", bounce: 0.5}}>hey, it's</motion.h1>
-            <motion.h1  initial = {{ opacity: 0, y: 100 }} animate = {{ opacity: 1, y: 0 }} transition = {{ duration: 1.5, ease: "backIn", type: "spring", bounce: 0.5}} style={{fontFamily: "Digitag", marginLeft: '1rem', marginTop: '2.5rem'}}>Rohan</motion.h1>
+            <motion.h1  initial = {{ opacity: 0, y: 100 }} animate = {{ opacity: 1, y: 0 }} transition = {{ duration: 1.5, ease: "backIn", type: "spring", bounce: 0.5}} style={{fontFamily: "Digitag", marginLeft: '1.5vw', marginTop: '6.8vh'}}>Rohan</motion.h1>
             </div>
             <motion.p  initial = {{ opacity: 0, y: 100 }} animate = {{ opacity: 1, y: 0 }} transition = {{ duration: 1.5, ease: "backIn", type: "spring", bounce: 0.5, delay: 0.25}}>
               i'm a sophomore at the university of illinois urbana-champaign. when i'm not thinking of what to eat next, producing music, or doomscrolling for my next favorite pair of jeans on depop, i'm a passionate software engineer, web developer, and data scientist.
             </motion.p>
-            <motion.div initial = {{ opacity: 0, y: 100 }} animate = {{ opacity: 1, y: 0 }} transition = {{ duration: 1.5, ease: "backIn", type: "spring", bounce: 0.5, delay: 0.25}}>
+            <motion.div style={{marginTop: '2vh'}} initial = {{ opacity: 0, y: 100 }} animate = {{ opacity: 1, y: 0 }} transition = {{ duration: 1.5, ease: "backIn", type: "spring", bounce: 0.5, delay: 0.25}}>
                 <LastListened />
             </motion.div>
             <motion.div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}  initial = {{ opacity: 0, y: 100 }} animate = {{ opacity: 1, y: 0 }} transition = {{ duration: 1.5, ease: "backIn", type: "spring", bounce: 0.5, delay: 0.5}}>
@@ -139,47 +139,47 @@ const Home = () => {
                 </h1>
                 <img className="click-me-arrow" src={sprayArrow}></img>
             </motion.div>
-            <motion.div className="uiuc-container" key="animation-on-state" initial={false} variants={variantsUIUC} animate={showExperience ? 'show' : 'hide'} whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} onClick={() => uiucContainerHeight == "12rem" ? setUiucContainerHeight("7.5rem") : setUiucContainerHeight("12rem")} style={{height: uiucContainerHeight, transition: "height 0.5s ease-out"}}>
+            <motion.div className="uiuc-container" key="animation-on-state" initial={false} variants={variantsUIUC} animate={showExperience ? 'show' : 'hide'} whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} onClick={() => uiucContainerHeight == "30vh" ? setUiucContainerHeight("25vh") : setUiucContainerHeight("30vh")} style={{height: uiucContainerHeight, transition: "height 0.5s ease-out"}}>
                 <p className="experience-p" style={{fontFamily: "Graffiti", fontSize: "1.0rem"}}>University of Illinois Urbana-Champaign</p>
                 <p className="experience-p">August 2023 - May 2026</p>
                 <p className="experience-p">Major: Statistics & Computer Science</p>
                 <p className="experience-p">GPA: 3.97/4.0</p>
                 <p className="experience-p">Courses: Data Structures, Algorithms, Computer Systems, Numerical Methods, & Statistical Modeling</p>
-                <Icon icon={"ic:baseline-play-arrow"} className="dropdown-arrow" style={{transform: uiucContainerHeight == "12rem" ? "rotate(90deg)" : "none"}}/>
+                <Icon icon={"ic:baseline-play-arrow"} className="dropdown-arrow" style={{transform: uiucContainerHeight == "30vh" ? "rotate(90deg)" : "none"}}/>
             </motion.div>
             <motion.div className="uiuc-line-horizontal" key="animation-on-state" initial={false} variants={variantsUIUC} animate={showExperience ? 'show' : 'hide'}/>
             <motion.div className="uiuc-line-vertical" key="animation-on-state" initial={false} variants={variantsUIUC} animate={showExperience ? 'show' : 'hide'}/>
             <motion.div className="uiuc-circle" key="animation-on-state" initial={false} variants={variantsUIUC} animate={showExperience ? 'show' : 'hide'}/>
 
-            <motion.div className="experience1-container" key="animation-on-state" initial={false} variants={variantsExperience1} animate={showExperience ? 'show' : 'hide'} whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} onClick={() => experience1ContainerHeight == "30rem" ? setExperience1ContainerHeight("7rem") : setExperience1ContainerHeight("30rem")} style={{height: experience1ContainerHeight, transition: "height 0.5s ease-out"}}>
+            <motion.div className="experience1-container" key="animation-on-state" initial={false} variants={variantsExperience1} animate={showExperience ? 'show' : 'hide'} whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} onClick={() => experience1ContainerHeight == "30vh" ? setExperience1ContainerHeight("7rem") : setExperience1ContainerHeight("30vh")} style={{height: experience1ContainerHeight, transition: "height 0.5s ease-out"}}>
                 <p className="experience-p" style={{fontFamily: "Graffiti", fontSize: "1rem"}}>Human-Computer Interaction Lab</p>
                 <p className="experience-p">Software Engineering Research Assistant</p>
                 <p className="experience-p">January 2024 - Present</p>
                 <p className="experience-p">Contributed to the DATA Peer Tool to evaluate the utility of data from GitHub, Slack, Google Docs to determine value of contributions by team members in CS 465, a project-based UIUC course.</p>
                 <p className="experience-p">Developed configurable dashboard web application using Qualtrics and Next.js to generate intelligent feedback on students’ teamwork behaviors, perform data-augmented peer evaluations, and enable assessment of teamwork processes.</p>
                 <p className="experience-p">Generated data visualizations using Python, Streamlit, and GitHub REST API to determine generate actionable insights on how pull requests, commits, diff files, etc. can be used to determine contribution impact of a team member.</p>
-                <Icon icon={"ic:baseline-play-arrow"} className="dropdown-arrow" style={{transform: experience1ContainerHeight == "30rem" ? "rotate(90deg)" : "none"}}/>
+                <Icon icon={"ic:baseline-play-arrow"} className="dropdown-arrow" style={{transform: experience1ContainerHeight == "30vh" ? "rotate(90deg)" : "none"}}/>
             </motion.div>
             <motion.div className="experience1-line-horizontal" key="animation-on-state" initial={false} variants={variantsExperience1} animate={showExperience ? 'show' : 'hide'}/>
             <motion.div className="experience1-line-vertical" key="animation-on-state" initial={false} variants={variantsExperience1} animate={showExperience ? 'show' : 'hide'}/>
             <motion.div className="experience1-circle" key="animation-on-state" initial={false} variants={variantsExperience1} animate={showExperience ? 'show' : 'hide'}/>
 
-            <motion.div className="experience2-container" key="animation-on-state" initial={false} variants={variantsExperience2} animate={showExperience ? 'show' : 'hide'} whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} onClick={() => experience2ContainerHeight == "23rem" ? setExperience2ContainerHeight("6.5rem") : setExperience2ContainerHeight("23rem")} style={{height: experience2ContainerHeight, transition: "height 0.5s ease-out"}}>
+            <motion.div className="experience2-container" key="animation-on-state" initial={false} variants={variantsExperience2} animate={showExperience ? 'show' : 'hide'} whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} onClick={() => experience2ContainerHeight == "30vh" ? setExperience2ContainerHeight("6.5rem") : setExperience2ContainerHeight("30vh")} style={{height: experience2ContainerHeight, transition: "height 0.5s ease-out"}}>
                 <p className="experience-p" style={{fontFamily: "Graffiti", fontSize: "1.3rem"}}>AM Best</p>
                 <p className="experience-p">Software Developer Intern</p>
                 <p className="experience-p">June 2024 - July 2024</p>
                 <p className="experience-p">Successfully designed and launched the Company Logo API for AM Best Rating Services using ASP.NET, C# and Azure DevOps, resulting in client logos being displayed on AMBest mobile app, public profile webpage, credit ratings, and financial reports.</p>
-                <p className="experience-p">Conducted unit and automated testing using MSTest and Azure Pipeline to ensure proper API functionality and
+                <p className="experience-p">Conducted unit and 30vhmated testing using MSTest and Azure Pipeline to ensure proper API functionality and
 ensured code security using Veracode.</p>
                 <p className="experience-p">Led technical team meetings, providing insights and feedback on project progress and potential improvements to
 business executives.</p>
-                <Icon icon={"ic:baseline-play-arrow"} className="dropdown-arrow" style={{transform: experience2ContainerHeight == "23rem" ? "rotate(90deg)" : "none"}}/>
+                <Icon icon={"ic:baseline-play-arrow"} className="dropdown-arrow" style={{transform: experience2ContainerHeight == "30vh" ? "rotate(90deg)" : "none"}}/>
             </motion.div>
             <motion.div className="experience2-line-horizontal" key="animation-on-state" initial={false} variants={variantsExperience2} animate={showExperience ? 'show' : 'hide'}/>
             <motion.div className="experience2-line-vertical" key="animation-on-state" initial={false} variants={variantsExperience2} animate={showExperience ? 'show' : 'hide'}/>
             <motion.div className="experience2-circle" key="animation-on-state" initial={false} variants={variantsExperience2} animate={showExperience ? 'show' : 'hide'}/>
 
-            <motion.div className="experience3-container" key="animation-on-state" initial={false} variants={variantsExperience3} animate={showExperience ? 'show' : 'hide'} whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} onClick={() => experience3ContainerHeight == "19rem" ? setExperience3ContainerHeight("6.5rem") : setExperience3ContainerHeight("19rem")} style={{height: experience3ContainerHeight, transition: "height 0.5s ease-out"}}>
+            <motion.div className="experience3-container" key="animation-on-state" initial={false} variants={variantsExperience3} animate={showExperience ? 'show' : 'hide'} whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} onClick={() => experience3ContainerHeight == "30vh" ? setExperience3ContainerHeight("6.5rem") : setExperience3ContainerHeight("30vh")} style={{height: experience3ContainerHeight, transition: "height 0.5s ease-out"}}>
                 <p className="experience-p" style={{fontFamily: "Graffiti", fontSize: "1.3rem"}}>Changing the Present</p>
                 <p className="experience-p">Front-End Developer Intern</p>
                 <p className="experience-p">November 2023 - January 2024</p>
